32d2b0028bf36b59a24266c30677c2f472b2dfd0
Diallo <mamadouadama609@gmail.com>
2024-06-04 15:44:09 -0500

    Completed the all pipepline and all tasks are running perfectly

--- dags/execute_sql_pipeline.py
+++ dags/execute_sql_pipeline.py
@@ -38,5 +38,59 @@ with DAG(
         dag = dag
     )
 
+    first_insert_values = SqliteOperator(
+        task_id = 'first_insert_values',
+        sql = r"""
+            INSERT INTO users (name, age, is_active) VALUES 
+                ('Julie', 30, false),
+                ('Peter', 55, true),
+                ('Emily', 37, false),
+                ('Katrina', 54, false),
+                ('Joseph', 27, true);
+        """,
+        sqlite_conn_id = 'my_sqlite_conn',
+        dag = dag
+    )
 
-create_table
+    second_insert_values =SqliteOperator(
+        task_id = 'second_insert_values',
+        sql = r"""
+            INSERT INTO users (name, age) VALUES 
+                ('Harry', 49),
+                ('Nancy', 52),
+                ('Elvis', 26),
+                ('Mia', 20);
+        """,
+        sqlite_conn_id = 'my_sqlite_conn',
+        dag = dag,
+    )
+
+    delete_values = SqliteOperator(
+        task_id = 'delete_values',
+        sql = r"""
+            DELETE FROM users WHERE is_active = 0;
+        """,
+        sqlite_conn_id = 'my_sqlite_conn',
+        dag = dag,
+    )
+
+    update_values = SqliteOperator(
+        task_id = 'update_values',
+        sql = r"""
+            UPDATE users SET city = 'Seattle';
+        """,
+        sqlite_conn_id = 'my_sqlite_conn',
+        dag = dag,
+    )
+
+
+    display_all_from_users = SqliteOperator(
+        task_id = 'display_result',
+        sql = r"""SELECT * FROM users""",
+        sqlite_conn_id = 'my_sqlite_conn',
+        dag = dag,
+        do_xcom_push = True
+    )
+
+
+create_table >> [first_insert_values, second_insert_values] >>  delete_values >> update_values >> display_all_from_users
